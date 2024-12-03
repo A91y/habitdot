b9ae22c050b950ad476a270036adc48782035f06
A91y <65825207+A91y@users.noreply.github.com>
2024-12-03 17:10:52 +0530

    refactor layout components for improved styling and readability

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ import { useUserStore } from "./store/user";
 import Explore from "./pages/Explore/ChallengeExplore";
 import { useNavigate } from "react-router-dom";
 
-
-
-
 function App() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
   const authRef = useRef(false);
@@ -19,10 +16,8 @@ function App() {
   const { setLoggedIn, loggedIn } = useUserStore();
   const navigate = useNavigate();
 
- 
-
   return (
-    <div className="w-screen h-safe flex flex-col justify-between bg-[#F8F8F8] overflow-hidden relative md:max-w-[380px]">
+    <div className="w-screen h-safe flex flex-col justify-between  overflow-hidden relative md:max-w-[380px]">
       {loggedIn ? <Explore /> : <Signup />}
     </div>
   );
