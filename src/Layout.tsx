e9c9e133b819c7953920fa03ff6e4bd250f60766
A91y <65825207+A91y@users.noreply.github.com>
2024-12-03 18:05:36 +0530

    add conditional background color to layout based on pathname

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,9 @@ const Layout = ({ children, showAppbar = true }) => {
   return (
     <div className="min-h-screen w-screen flex flex-col justify-between ">
       <div
-        className={`flex-1 w-full md:max-w-[380px] md:mx-auto overflow-y-auto md:border-2 md:border-[#efefef50] md:min-h-screen pb-[70px] bg-white`}
+        className={`flex-1 w-full md:max-w-[380px] md:mx-auto overflow-y-auto md:border-2 md:border-[#efefef50] md:min-h-screen pb-[70px] ${
+          pathname == "/" ? "bg-[#FFE668]" : "bg-white"
+        }`}
       >
         {children}
         <div className="">{showAppbar && <Appbar />}</div>
